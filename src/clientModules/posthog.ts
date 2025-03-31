54e26bd5902d74476b98d33755f68095c8946aad
Nathan Perkins <nathanjp91@gmail.com>
2025-03-31 11:49:00 +0100

    Update Docs from SDK (#84)
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Add docs for MCP server integration (#70)
    
    * MCP docs
    
    * Link
    
    * Use **↗** for links
    
    * Improve wording
    
    * Try removing docs
    
    * Docs PR comments
    
    * Add os import
    
    * Use os.getenv
    
    * Fix MCP doc tests
    
    * Add docs on tool output schema (#66)
    
    * Add docs on tool output schema
    
    * Update docs/product/Extend and run tools/Adding custom tools.md
    
    Co-authored-by: Sam Stephens <sam@portialabs.ai>
    
    ---------
    
    Co-authored-by: Sam Stephens <sam@portialabs.ai>
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Fix clarification docs (#72)
    
    * Add docs on tool output schema
    
    * Fix clarifications docs
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Mistral dep fix (#75)
    
    * Mistral dep fix
    
    * Name mistral extra explicitly
    
    ---------
    
    Co-authored-by: Sam Stephens <sam@portialabs.ai>
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Update docs from SDK [skip ci]
    
    * Add getting started tabs for Gemini and Azure OpenAI providers (#79)
    
    * Add getting started tabs for Gemini and Azure OpenAI
    
    * Remove ::Note:: inside tabitem
    
    * Add [all] install doc
    
    * Config for testing
    
    * Clarify
    
    * Update pyproject.toml
    
    * [POR-1061] -  Add Security Page (#76)
    
    * Add security page
    
    * imrpovemnts
    
    * first line
    
    * Add GOOGLE_API_KEY to schedule action check (#81)
    
    * Add posthog (#80)
    
    * add posthog
    
    * client only
    
    * fixed up
    
    * clean up
    
    * fix lint error
    
    * Update example plan with conditions (#82)
    
    * fix for extras
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Sam Stephens <sam@portialabs.ai>
    Co-authored-by: robbie-portia <robbie@portialabs.ai>
    Co-authored-by: emmaportia <emma@portialabs.ai>
    Co-authored-by: TomSPortia <tom@portialabs.ai>
    Co-authored-by: Richard Gill <richardgill@users.noreply.github.com>
    Co-authored-by: Omar ElMohandes <OmarEl-Mohandes@users.noreply.github.com>

---
+++ src/clientModules/posthog.ts
@@ -0,0 +1,38 @@
+// @ts-expect-error
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+import Cookies from "js-cookie";
+import posthog from "posthog-js";
+
+// WARNING: These cookies are used by the app. Make sure you consider both when making changes.
+export const ESSENTIAL_COOKIES = "essential-cookies";
+export const NON_ESSENTIAL_COOKIES = "non-essential-cookies";
+
+export const getEssentialCookies = () => {
+  return Cookies.get(ESSENTIAL_COOKIES) === "true";
+};
+
+export const getNonEssentialCookies = () => {
+  return Cookies.get(NON_ESSENTIAL_COOKIES) === "true";
+};
+
+if (ExecutionEnvironment.canUseDOM) {
+  posthog.init(
+    process.env.PUBLIC_POSTHOG_KEY ??
+      "phc_QHjx4dKKNAqmLS1U64kIXo4NlYOGIFDgB1qYxw3wh1W", // dev posthog token
+    {
+      persistence: getNonEssentialCookies() ? "localStorage+cookie" : "memory",
+      cross_subdomain_cookie: true,
+      api_host: "https://eu.i.posthog.com",
+      ui_host: "https://eu.posthog.com",
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually below
+    },
+  );
+}
+
+// Inspired by https://github.com/PostHog/posthog-docusaurus/blob/master/src/posthog.js
+export const onRouteUpdate = ({ location, previousLocation }) => {
+  if (!ExecutionEnvironment.canUseDOM) return;
+  if (location.pathname != previousLocation?.pathname) {
+    posthog.capture("$pageview");
+  }
+};
